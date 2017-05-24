9726a758161d383eedeff3556f8edb1e1a653fab
André Hamel <andrehamel@users.noreply.github.com>
2017-05-24 15:51:55 -0400

    First commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,7 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    //OK
 }
 
 //implement new functions here
